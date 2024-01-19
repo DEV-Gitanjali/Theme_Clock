f84709751006acaa011c71f7e7be947ca9e900d3
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-19 09:45:40 +0530

    this code for how to function a clock

--- script.js
+++ script.js
@@ -11,5 +11,55 @@ const toggle =document.querySelector('.toggle')
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+// if you click darkmode  button  html add dark & innerhtml is light mode 
+toggle.addEventListener('click',(e)=>{
+    const html=document.querySelector('html')
+    
+    if(html.classList.contains('dark'))
+    {
+        html.classList.remove('dark')
+        e.target.innerHTML='Dark mode'
+    }
+
+    else{
+        html.classList.add('dark')
+        e.target.innerHTML='Light mode'
+    }
+
+})
+
+// this method for set time all the element 
+function setTime(){
+    const time=new Date();
+    const month=time.getMonth()
+    const day=time.getDay()
+    const date=time.getDate()
+    const hours=time.getHours()
+    const hoursForClock=hours >=13? hours % 12:hours
+    const minutes=time.getMinutes()
+    const seconds=time.getSeconds()
+    const ampm=hours >=12 ? 'PM' : 'AM'
+
+    // this method rotate hour  clock 360 deg
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+    
+
+}
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+// it will be rotate every 1sec
+setInterval(setTime, 1000)
+
+
 
 
